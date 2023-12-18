33bf5ad181e859fad71b821a673d8eeb604be954
Ivan Iakovlev <Shapuiz35@outlook.com>
2023-12-18 16:10:45 +0300

    Исправлена работа с кастомными стилями, добавлен кастомный стиль курсора

--- Summary table - detalization page/customOpenDetalizationPage.js
+++ Summary table - detalization page/customOpenDetalizationPage.js
@@ -4,20 +4,32 @@ const mainTableGUID = '0661ceb0deec4e7ba60ef703f9697c08'; //The GUID of the widg
 const detailPageID = 1; //The number of the detail page. Starts from 0
 const detailPageFilterGUID = '2ed48402e64b4f1c88eab04f50999efa'; //
 
-//Code block bellow (Do not change)!
-$('div [id="' + mainTableGUID + '"]').click(function () {
+let olapTableRender = OlapTableRender({
+  general: w.general,
+  pivotGridOptions: w.pivotGridOptions,
+  style: w.style,
+  errorState: w.errorState,
+  textFormatters: w.textFormatters,
+});
+
+olapTableRender.pivotGridInstance.option('onContentReady', function () {
   let lastDrilldownLevelCells = $('div [id="' + mainTableGUID + '"]')
     .find('.dx-last-cell')
     .not('.dx-pivotgrid-collapsed');
+
+  //add custom cursor styles
+  let newStyleValue = $(lastDrilldownLevelCells).attr('style') + 'cursor: pointer;';
+  lastDrilldownLevelCells.attr('style', newStyleValue);
 
   //Remove old event listeners
   lastDrilldownLevelCells.off('click');
 
   //Add new event listeners
   lastDrilldownLevelCells.on('click', function (info) {
-    let currentTargetElementValue = $(info.currentTarget).children('span')[0].innerHTML;
+    let currentTargetElementValue = $($(info.currentTarget).children('span')[0]).text();
+    console.warn(currentTargetElementValue);
     visApi().setFilterSelectedValues(detailPageFilterGUID, [[currentTargetElementValue]]);
     $('button')[detailPageID].click();
-    console.log(clickedElementValue.innerTEXT);
   });
 });
+olapTableRender; //эта строчка всегда должна оставаться последней
